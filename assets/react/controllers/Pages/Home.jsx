2f7e64c2f4166fad4713f637d64ed3d6cfcc2888
laynaria <pro.laynaria@outlook.fr>
2024-06-29 21:04:34 +0200

    home page now show articles from backend

--- assets/react/controllers/Pages/Home.jsx
+++ assets/react/controllers/Pages/Home.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { ThemeContext } from "../context/ThemeContext";
 import Card from "../components/Card";
+import axios from "axios";
 
 export default function Home(props) {
     const { theme } = useContext(ThemeContext);
@@ -14,49 +15,15 @@ export default function Home(props) {
                 "koef kopfezop efzoekpz fzegopeg opj eojuzejfopzejp e zzfezkez eze^pez^z",
             image: "https://picsum.photos/id/10/498/200",
         },
-        {
-            id: 2,
-            title: "blabla 2",
-            description:
-                "koef kopfezop efzoekpz fzegopeg opj eojuzejfopzejp e zzfezkez eze^pez^z",
-            image: "https://picsum.photos/id/237/498/200",
-        },
-        {
-            id: 3,
-            title: "blabla 3",
-            description:
-                "koef kopfezop efzoekpz fzegopeg opj eojuzejfopzejp e zzfezkez eze^pez^z",
-            image: "https://picsum.photos/id/29/498/200",
-        },
-        {
-            id: 4,
-            title: "blabla 4",
-            description:
-                "koef kopfezop efzoekpz fzegopeg opj eojuzejfopzejp e zzfezkez eze^pez^z",
-            image: "https://picsum.photos/id/17/498/200",
-        },
-        {
-            id: 5,
-            title: "blabla 5",
-            description:
-                "koef kopfezop efzoekpz fzegopeg opj eojuzejfopzejp e zzfezkez eze^pez^z",
-            image: "https://picsum.photos/id/119/498/200",
-        },
-        {
-            id: 6,
-            title: "blabla 6",
-            description:
-                "koef kopfezop efzoekpz fzegopeg opj eojuzejfopzejp e zzfezkez eze^pez^z",
-            image: "https://picsum.photos/id/50/498/200",
-        },
-        {
-            id: 7,
-            title: "blabla 7",
-            description:
-                "koef kopfezop efzoekpz fzegopeg opj eojuzejfopzejp e zzfezkez eze^pez^z",
-            image: "https://picsum.photos/id/50/498/200",
-        },
     ]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await axios.get("/api/articles");
+            setArticles(data);
+        };
+        fetchData();
+    }, []);
 
     return (
         <section>
